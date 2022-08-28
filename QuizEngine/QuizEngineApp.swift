29cd7c31ecb31144c44de8cb6518aaaa31fa1d89
chavesyuri-mb <79917740+chavesyuri-mb@users.noreply.github.com>
2022-08-28 11:39:46 -0400

    Initial Commit

---
+++ QuizEngine/QuizEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizEngineApp.swift
+//  QuizEngine
+//
+//  Created by Yuri Chaves on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct QuizEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
